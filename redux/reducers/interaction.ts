e8e32fa95224d5b31d7a51f843e1e2cfb02dda10
Canoral <kaya68@outlook.com>
2023-11-12 19:32:46 +0100

    first commit

---
+++ redux/reducers/interaction.ts
@@ -0,0 +1,19 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+interface InteractionState {
+  test: string | null;
+}
+
+const initialState: InteractionState = {
+  test: null,
+};
+
+export const testAction = createAction<string>("interaction/test");
+
+const interactionReducer = createReducer(initialState, (builder) => {
+  builder.addCase(testAction, (state, action) => {
+    state.test;
+  });
+});
+
+export default interactionReducer;
