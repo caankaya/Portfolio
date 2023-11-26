59fc9bb4b478ac0c68f7aff66a2b7a146ad85583
Canoral <kaya68@outlook.com>
2023-11-26 21:10:29 +0100

    fix: Correction responsive design du Technos en smartphone

--- src/Technos/Technos.tsx
+++ src/Technos/Technos.tsx
@@ -2,15 +2,15 @@ export default function Technos() {
   return (
     <div className="w-full" id="technos">
       <h2 className="mb-8 sm:mb-10 text-4xl text-center font-extrabold">Technologies</h2>
-      <div className="flex md:justify-between max-sm:flex-wrap max-sm:gap-x-20">
+      <div className="flex justify-around max-sm:flex-wrap max-sm:gap-x-5">
         <div className="max-sm:mb-10">
-          <h3 className="text-2xl font-bold">Design</h3>
+          <h3 className="text-2xl font-bold max-sm:text-xl">Design</h3>
           <ul className="flex flex-col gap-2 mt-4">
-            <li className="flex items-center gap-2 text-sm">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
               <svg
                 fill="#ffffff"
-                width="32px"
-                height="32px"
+                width="25px"
+                height="25px"
                 viewBox="0 0 24.00 24.00"
                 xmlns="http://www.w3.org/2000/svg"
                 xmlSpace="preserve"
@@ -30,8 +30,8 @@ export default function Technos() {
               </svg>
               Tailwind
             </li>
-            <li className="flex items-center gap-2 text-sm">
-              <svg fill="#ffffff" width="32px" height="32px" viewBox="0 -4 32 32" xmlns="http://www.w3.org/2000/svg">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
+              <svg fill="#ffffff" width="25px" height="25px" viewBox="0 -4 32 32" xmlns="http://www.w3.org/2000/svg">
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
@@ -40,10 +40,10 @@ export default function Technos() {
               </svg>
               SaSS
             </li>
-            <li className="flex items-center gap-2 text-sm">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
               <svg
-                width="32px"
-                height="32px"
+                width="25px"
+                height="25px"
                 viewBox="0 0 76 76"
                 xmlns="http://www.w3.org/2000/svg"
                 xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -70,13 +70,13 @@ export default function Technos() {
           </ul>
         </div>
         <div className="max-sm:mb-10">
-          <h3 className="text-2xl font-bold text-se">Front-end</h3>
+          <h3 className="text-2xl font-bold max-sm:text-xl">Front-end</h3>
           <ul className="flex flex-col gap-2 mt-4">
-            <li className="flex items-center gap-2 text-sm">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
               <svg
                 fill="#ffffff"
-                width="32px"
-                height="32px"
+                width="25px"
+                height="25px"
                 viewBox="0 0 14 14"
                 role="img"
                 focusable="false"
@@ -91,8 +91,8 @@ export default function Technos() {
               </svg>
               Typescript
             </li>
-            <li className="flex items-center gap-2 text-sm">
-              <svg width="32px" height="32px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
+              <svg width="25px" height="25px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
@@ -100,10 +100,10 @@ export default function Technos() {
                   <path d="M12,10.11A1.87,1.87,0,1,1,10.13,12,1.88,1.88,0,0,1,12,10.11M7.37,20c.63.38,2-.2,3.6-1.7a24.22,24.22,0,0,1-1.51-1.9A22.7,22.7,0,0,1,7.06,16c-.51,2.14-.32,3.61.31,4m.71-5.74-.29-.51a7.91,7.91,0,0,0-.29.86c.27.06.57.11.88.16l-.3-.51m6.54-.76.81-1.5-.81-1.5c-.3-.53-.62-1-.91-1.47C13.17,9,12.6,9,12,9s-1.17,0-1.71,0c-.29.47-.61.94-.91,1.47L8.57,12l.81,1.5c.3.53.62,1,.91,1.47.54,0,1.11,0,1.71,0s1.17,0,1.71,0c.29-.47.61-.94.91-1.47M12,6.78c-.19.22-.39.45-.59.72h1.18c-.2-.27-.4-.5-.59-.72m0,10.44c.19-.22.39-.45.59-.72H11.41c.2.27.4.5.59.72M16.62,4c-.62-.38-2,.2-3.59,1.7a24.22,24.22,0,0,1,1.51,1.9,22.7,22.7,0,0,1,2.4.36c.51-2.14.32-3.61-.32-4m-.7,5.74.29.51a7.91,7.91,0,0,0,.29-.86c-.27-.06-.57-.11-.88-.16l.3.51m1.45-7c1.47.84,1.63,3.05,1,5.63,2.54.75,4.37,2,4.37,3.68s-1.83,2.93-4.37,3.68c.62,2.58.46,4.79-1,5.63s-3.45-.12-5.37-1.95c-1.92,1.83-3.91,2.79-5.38,1.95s-1.62-3-1-5.63c-2.54-.75-4.37-2-4.37-3.68S3.08,9.07,5.62,8.32c-.62-2.58-.46-4.79,1-5.63s3.46.12,5.38,1.95c1.92-1.83,3.91-2.79,5.37-1.95M17.08,12A22.51,22.51,0,0,1,18,14.26c2.1-.63,3.28-1.53,3.28-2.26S20.07,10.37,18,9.74A22.51,22.51,0,0,1,17.08,12M6.92,12A22.51,22.51,0,0,1,6,9.74c-2.1.63-3.28,1.53-3.28,2.26S3.93,13.63,6,14.26A22.51,22.51,0,0,1,6.92,12m9,2.26-.3.51c.31,0,.61-.1.88-.16a7.91,7.91,0,0,0-.29-.86l-.29.51M13,18.3c1.59,1.5,3,2.08,3.59,1.7s.83-1.82.32-4a22.7,22.7,0,0,1-2.4.36A24.22,24.22,0,0,1,13,18.3M8.08,9.74l.3-.51c-.31,0-.61.1-.88.16a7.91,7.91,0,0,0,.29.86l.29-.51M11,5.7C9.38,4.2,8,3.62,7.37,4s-.82,1.82-.31,4a22.7,22.7,0,0,1,2.4-.36A24.22,24.22,0,0,1,11,5.7Z"></path>{" "}
                 </g>
               </svg>
-              React & React Native
+              React & Native
             </li>
-            <li className="flex items-center gap-2 text-sm">
-              <svg fill="#ffffff" width="32px" height="32px" viewBox="0 -0.5 25 25" xmlns="http://www.w3.org/2000/svg">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
+              <svg fill="#ffffff" width="25px" height="25px" viewBox="0 -0.5 25 25" xmlns="http://www.w3.org/2000/svg">
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
@@ -117,16 +117,16 @@ export default function Technos() {
           </ul>
         </div>
         <div className="max-sm:mb-10">
-          <h3 className="text-2xl font-bold">Back-end</h3>
+          <h3 className="text-2xl font-bold max-sm:text-xl">Back-end</h3>
           <ul className="flex flex-col gap-2 mt-4">
-            <li className="flex items-center gap-2 text-sm">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
               <svg
                 fill="#ffffff"
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
                 xmlnsXlink="http://www.w3.org/1999/xlink"
-                width="32px"
-                height="32px"
+                width="25px"
+                height="25px"
                 viewBox="0 0 512 512"
                 enableBackground="new 0 0 512 512"
                 xmlSpace="preserve"
@@ -148,11 +148,11 @@ export default function Technos() {
               </svg>
               Nodejs
             </li>
-            <li className="flex items-center gap-2 text-sm">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
               <svg
                 fill="#ffffff"
-                width="32px"
-                height="32px"
+                width="25px"
+                height="25px"
                 viewBox="0 0 32.00 32.00"
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
@@ -169,8 +169,8 @@ export default function Technos() {
               </svg>
               PostgreSQL
             </li>
-            <li className="flex items-center gap-2 text-sm">
-              <svg width="32px" height="32px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
+              <svg width="25px" height="25px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
@@ -254,12 +254,12 @@ export default function Technos() {
           </ul>
         </div>
         <div>
-          <h3 className="text-2xl font-bold">Autres</h3>
+          <h3 className="text-2xl font-bold max-sm:text-xl">Autres</h3>
           <ul className="flex flex-col gap-2 mt-4">
-            <li className="flex items-center gap-2 text-sm">
+            <li className="flex items-center gap-2 text-sm max-sm:text-xs">
               <svg
-                width="32px"
-                height="32px"
+                width="25px"
+                height="25px"
                 viewBox="0 0 20 20"
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
