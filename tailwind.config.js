a335f77ff381218dbdbf69e18e921eac7b5cf9b5
Canoral <kaya68@outlook.com>
2023-11-28 12:35:07 +0100

    feat: :lipstick: Changement de thème

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["aqua"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ["dracula"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
